256689f860a5ea6c8794c2546624b971ab330ed9
tdegold <49491595+tdegold@users.noreply.github.com>
2021-08-19 11:17:50 +0200

    added project

---
+++ ZPL_Net5/Program.cs
@@ -0,0 +1,14 @@
+﻿using Neodynamic.SDK.ZPLPrinter;
+
+namespace ZPL_Net5
+{
+    class Program
+    {
+        const string _licenseKey = "";
+        const string _licenseOwner = "";
+        static void Main(string[] args)
+        {
+            var myZPLPrinter = new ZPLPrinter(_licenseOwner, _licenseKey);
+        }
+    }
+}
---
+++ ZPL_NetCode3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Neodynamic.SDK.ZPLPrinter;
+
+namespace ZPL_NetCode3._1
+{
+    class Program
+    {
+        const string _licenseKey = "";
+        const string _licenseOwner = "";
+
+        static void Main(string[] args)
+        {
+            var myZPLPrinter = new ZPLPrinter(_licenseOwner, _licenseKey);
+        }
+    }
+}
